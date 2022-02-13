4f10c96729b80618a9aa0d3f8c9597173a95ac1b
Vinícius Gutierrez <srgutyerrez@gmail.com>
2022-02-13 16:09:28 -0300

    feat: (✪‿✪)ノ first commit

---
+++ lib/index.ts
@@ -0,0 +1,19 @@
+export abstract class Exception extends Error {
+  public status!: number
+
+  constructor(
+    status: number,
+    message: string
+  ) {
+    super(message)
+
+    this.status = status
+  }
+}
+
+export class FileNotFoundException extends Exception {
+  constructor(
+    status: number = 404,
+    message: string = 'file not found'
+  ) { super(status, message) }
+}
